45d1c6b4f6b93f7f42d9763a1403d773fb46bc39
Marianna <maryanarez@gmail.com>
2024-03-14 14:04:42 +0100

    Landing Page

--- todo-client/src/pages/Landing/Landing.jsx
+++ todo-client/src/pages/Landing/Landing.jsx
@@ -1,7 +1,23 @@
-export default function Landing() {
+import { Link } from "react-router-dom";
+import styles from "./Landing.module.css";
+
+function Landing() {
   return (
-    <>
-      <div>This is a landing page</div>
-    </>
+    <div>
+      <main className={styles.landing}>
+        <nav className={styles.landingBtnContainer}>
+          <Link to="/login">
+            <button className={styles.landingBtnLogin}>Log In</button>
+          </Link>
+          <Link to="/register">
+            <button className={styles.landingBtnCreate}>
+              Create an Account
+            </button>
+          </Link>
+        </nav>
+      </main>
+    </div>
   );
 }
+
+export default Landing;
