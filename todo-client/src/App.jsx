0f1d6951c2657608d9f0c75f37eee12ecfe925aa
Quantinx <josephtassell@gmail.com>
2024-03-14 10:57:01 +0100

    created framework for pages

--- todo-client/src/App.jsx
+++ todo-client/src/App.jsx
@@ -1,13 +1,24 @@
 import TodoPage from "./pages/TodoPage/TodoPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Landing from "./pages/Landing/Landing";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-      <TodoPage />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/todo" element={<TodoPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
---
+++ todo-client/src/pages/Landing/Landing.jsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <>
+      <div>This is a landing page</div>
+    </>
+  );
+}
