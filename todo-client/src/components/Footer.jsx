6aa8e7a3ec7d3bd4c7cefe902f50422879de179a
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-03-14 14:06:12 +0100

    header and footer

--- todo-client/src/components/Footer.jsx
+++ todo-client/src/components/Footer.jsx
@@ -1,3 +1,66 @@
+import "./Footer.css";
+import { IoLogoGithub } from "react-icons/io";
+
 export default function Footer() {
-  return <footer className="footer"> Footer content here</footer>;
+  return (
+    <footer className="footer">
+      <h3 className="footer__title">
+        Built during &quot;beyond endpoint&quot;
+      </h3>
+      <nav>
+        <ul>
+          <li className="footer__link">
+            <a
+              className="footer__box-link"
+              href="https://github.com/Quantinx"
+              target="blank"
+            >
+              <p className="footer__link-text">Joe Tassell</p>
+              <IoLogoGithub className="footer__icon" />
+            </a>
+          </li>
+          <li className="footer__link">
+            <a
+              className="footer__box-link"
+              href="https://github.com/marianordqvist"
+              target="blank"
+            >
+              <p className="footer__link-text">Maria Nordqvist</p>
+              <IoLogoGithub className="footer__icon" />
+            </a>
+          </li>
+          <li className="footer__link">
+            <a
+              className="footer__box-link"
+              href="https://github.com/maryana-reznychenko"
+              target="blank"
+            >
+              <p className="footer__link-text">Maryana Reznychenko</p>
+              <IoLogoGithub className="footer__icon" />
+            </a>
+          </li>
+          <li className="footer__link">
+            <a
+              className="footer__box-link"
+              href="https://github.com/Wallenborg"
+              target="blank"
+            >
+              <p className="footer__link-text">Niklas Wallenborg</p>
+              <IoLogoGithub className="footer__icon" />
+            </a>
+          </li>
+          <li className="footer__link">
+            <a
+              className="footer__box-link"
+              href="https://github.com/vsafonova"
+              target="blank"
+            >
+              <p className="footer__link-text">Viktoriia Safonova</p>
+              <IoLogoGithub className="footer__icon" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
 }
--- todo-client/src/components/Header.jsx
+++ todo-client/src/components/Header.jsx
@@ -1,3 +1,14 @@
+import "./Header.css";
+
 export default function Header() {
-  return <header className="header">Header content here</header>;
+  return (
+    <header className="box_header">
+      <div className="header">
+        <h1 className="header__title">TASKIFY</h1>
+        <p className="header__subtitle">
+          - Simplify Your To-Do's, Amplify Your Productivity
+        </p>
+      </div>
+    </header>
+  );
 }
