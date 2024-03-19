5c33d1bf7168a6456df6215a0e2eb84a3638363b
vsafonova <viktoriyablak@gmail.com>
2024-03-19 13:09:57 +0100

    initial commit

--- todo-client/src/components/Filter.jsx
+++ todo-client/src/components/Filter.jsx
@@ -1,3 +1,12 @@
-export default function Filter() {
-  return <></>;
+export default function Filter({ onFilterChange }) {
+  return (
+    <div className="filter">
+      <select className="filter-select" onChange={onFilterChange} defaultValue="all">
+        <option value="today">Today</option>
+        <option value="tommorow">Tomorrow</option>
+        <option value="week">Next 7 Days</option>
+        <option value="all">All</option>
+      </select>
+    </div>
+  );
 }
--- todo-client/src/provider/TaskProvider.jsx
+++ todo-client/src/provider/TaskProvider.jsx
@@ -1,14 +1,12 @@
 import { useState, createContext } from "react";
 import { tasks } from "../testdata";
 export const TaskProviderContext = createContext();
-const data = tasks.slice(0, 5);
+const data = tasks;
 
 export const TaskProvider = ({ children }) => {
   const [taskItems, setTaskItems] = useState(data);
 
   function updateItem(id, title, desc, date, comp) {
-    console.log("updateing item " + id);
-    console.log(title, desc, date, comp);
     const updatedRow = taskItems.map((task) =>
       task.id === id
         ? {
