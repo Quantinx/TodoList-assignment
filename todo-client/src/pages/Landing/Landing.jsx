93bc2063ef786586fe514b108b2de40801c91da7
Marianna <maryanarez@gmail.com>
2024-03-23 13:21:37 +0100

    Added: 1. default styling to index.css 2. fonts and <meta> deskription as h1 3. Landing page transformed, but not finished.

--- todo-client/src/pages/Landing/Landing.jsx
+++ todo-client/src/pages/Landing/Landing.jsx
@@ -5,13 +5,15 @@ function Landing() {
   return (
     <div>
       <main className={styles.landing}>
+        <h1>A simple ToDo list to manage daily routine.</h1>
+        <h2>You are always ready to plan the day!</h2>
         <nav className={styles.landingBtnContainer}>
           <Link to="/login">
-            <button className={styles.landingBtnLogin}>Log In</button>
+            <button className={styles.landingBtnLogin}>log in</button>
           </Link>
           <Link to="/register">
             <button className={styles.landingBtnCreate}>
-              Create an Account
+              create an account
             </button>
           </Link>
         </nav>
