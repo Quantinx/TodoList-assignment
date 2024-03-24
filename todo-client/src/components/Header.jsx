ff3fd7e6ad480df1959743ce4d5212afd4aff6ef
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-03-24 12:14:52 +0100

    Fixe css -footer, header, loginform, registerform

--- todo-client/src/components/Header.jsx
+++ todo-client/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import "./Header.css";
+import { GiNotebook } from "react-icons/gi";
 
 export default function Header() {
   return (
     <header className="box_header">
       <div className="header">
         <h1 className="header__title">TASKIFY</h1>
-        <p className="header__subtitle">
+        <GiNotebook className="header-icon" />
+        {/* <p className="header__subtitle">
           - Simplify Your To-Do's, Amplify Your Productivity
-        </p>
+        </p> */}
       </div>
     </header>
   );
